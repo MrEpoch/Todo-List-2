dc3b25dbb267bc7db367dffda3d2f2d765c28d6a
MrEpoch <109413478+MrEpoch@users.noreply.github.com>
2023-02-20 22:01:45 +0100

    Started to think about how to design it, i am heavily inspired by how looks certain completed project on odin, i like how simple that work looks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as create from "./components/methods.js";
 
 const src = create.divCreate("", "src");
 let allFolder = {};
+let displayFolder = "main";
 
 function loadFolderCheck() {
   if (JSON.parse(localStorage.getItem("Todo-List-2")) === null) {
@@ -13,45 +14,56 @@ function loadFolderCheck() {
   }
 }
 
+console.log(JSON.parse(localStorage.getItem("Todo-List-2")).main);
+
 loadFolderCheck();
 
 function createSideBar() {
   const sideBar = create.divCreate("", "sidebar");
-  const main = create.h3Create("main", "side-main");
+  const nameCont = create.divCreate("", "side-name-container");
+  const name = create.h3Create("Todo App", "side-name");
+  const folderName = create.h3Create("main", "folder-name");
+  const thisWeek = create.h3Create("This week", "this-week");
 
   function createFolderControl() {
     const controlBar = create.divCreate("", "control-folder");
-    const curFolder = create.h4Create("", "current-folder");
-    const deleteFolder = create.divCreate("", "delete-folder");
-    const addFolder = create.divCreate("", "add-folder");
-    controlBar.append(curFolder, addFolder, deleteFolder);
+    const deleteFolder = create.divCreate("del", "delete-folder");
+    const addFolder = create.divCreate("add", "add-folder");
+    controlBar.append(addFolder, deleteFolder);
     return controlBar;
   }
 
   function createAddBar() {
     const bar = create.divCreate("", "bar-add");
-    const addNew = create.divCreate("", "add");
-    const deleteOne = create.divCreate("", "delete");
 
-    bar.append(addNew, deleteOne);
+    bar.append();
     return bar;
   }
 
   const addBar = createAddBar();
   const folderControl = createFolderControl();
+  nameCont.append(name);
 
-  sideBar.append(main, folderControl, addBar);
+  sideBar.append(nameCont, folderName, thisWeek, folderControl, addBar);
   return sideBar;
 }
 
 function createMainBody() {
   const main = create.divCreate("", "main-container");
+  const folderName = create.h2Create("main", "main-name");
+  const workMiddle = create.divCreate("", "main-middle");
 
+  workMiddle.append(folderName);
+  main.append(workMiddle);
   return main;
 }
 
 // temporary
-src.append(createSideBar());
-//
+src.append(createSideBar(), createMainBody());
 
+src.querySelector(".side-name").addEventListener("click", () => {
+  window.location.reload();
+});
+
+//
 document.body.append(src);
